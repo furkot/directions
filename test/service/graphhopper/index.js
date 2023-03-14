e274d6e090f396e677d1fbf9b2e990b57bad06be
Natalia Kowalczyk <melitele@code42day.com>
2023-03-14 20:33:43 +0100

    detect ferry segments in Graphhopper directions

--- test/service/graphhopper/index.js
+++ test/service/graphhopper/index.js
@@ -74,4 +74,58 @@ describe('graphhopper directions', function () {
     result.segments.reduce(function (len, seg) { return len + seg.path.length - 1; }, 0).should.equal(5749);
     result.should.have.property('provider', 'graphhopper');
   });
+
+  it('ferry', async function () {
+
+    response = require('./fixtures/ferry');
+
+    const query = {
+      ...directionsQuery,
+      points: [
+        [18.936008142490863, 54.33379650788689],
+        [18.94377850198444, 54.3330051697059]
+      ],
+      turnbyturn: true,
+      path: pathType.full
+    };
+    const result = await directions(query);
+    should.exist(result);
+    result.should.have.property('routes').with.length(1);
+    result.routes[0].should.have.property('ferry').eql(true);
+    result.should.have.property('segments').with.length(4);
+    result.segments[0].should.not.have.property('mode');
+    result.segments[0].should.have.property('path').with.length(7);
+    result.segments[1].should.have.property('mode', 6);
+    result.segments[1].should.have.property('path').with.length(2);
+    result.segments[2].should.not.have.property('mode');
+    result.segments[2].should.have.property('path').with.length(7);
+    result.segments[3].should.not.have.property('mode');
+    result.segments[3].should.have.property('path').with.length(2);
+    result.should.have.property('provider', 'graphhopper');
+  });
+
+  it('other ferry', async function () {
+
+    response = require('./fixtures/ferry-2');
+
+    const query = {
+      ...directionsQuery,
+      points: [
+        [1.7451973302237036, 50.936555851689576],
+        [1.2756059870033596, 51.110532072304125]
+      ],
+      turnbyturn: true,
+      path: pathType.full
+    };
+    const result = await directions(query);
+    should.exist(result);
+    result.should.have.property('routes').with.length(1);
+    result.routes[0].should.have.property('ferry').eql(true);
+    result.should.have.property('segments').with.length(2);
+    result.segments[0].should.have.property('mode', 6);
+    result.segments[0].should.have.property('path').with.length(79);
+    result.segments[1].should.not.have.property('mode');
+    result.segments[1].should.have.property('path').with.length(2);
+    result.should.have.property('provider', 'graphhopper');
+  });
 });
